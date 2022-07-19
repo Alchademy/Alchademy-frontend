4e0b631639ac1adaa97fce27899911c8b3a1eee2
Riley Hoffman <rileyjhoff@gmail.com>
2022-07-19 13:00:03 -0700

    initial setup for AccountPage

--- src/components/AccountPage.js
+++ src/components/AccountPage.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useStateContext } from '../StateProvider';
+import './AccountPage.css';
 
 export default function AccountPage() {
+  const { user, syllabus, assignment } = useStateContext();
+  console.log(syllabus);
+  console.log(assignment);
+
   return (
-    <div>Account</div>
+    <div className="account-page">
+      <div className="avatar-and-username">
+        {user.avatar && (
+          <div className="avatar">
+            <img src={user.avatar} />
+          </div>
+        )}
+        <div className="username">
+          {user.username} - {user.role === 4 && 'Admin'}
+          {user.role === 3 && 'Instructor'}
+          {user.role === 2 && 'TA'}
+          {user.role === 1 && 'Student'}
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   );
 }
