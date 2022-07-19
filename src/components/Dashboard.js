77bf8ea437c7dbf40d8f3251e2c3aefd2b25b474
Beau Elliott <belliott1516@gmail.com>
2022-07-19 10:02:27 -0700

    added syllabus titles to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getSyllabusByUserID } from '../services/fetch-syllabus';
+import { useStateContext } from '../StateProvider';
 
 export default function Dashboard() {
+  const { user, setUser } = useStateContext();
+  const [syllabus, setSyllabus] = useState([]);
+
+  useEffect(() => {
+    async function getSyllabus() {
+      const syllabusList = await getSyllabusByUserID(user.id);
+      if (syllabusList.length > 0) setSyllabus(syllabusList);
+    }
+    getSyllabus();
+  }, [user.id]);
+
   return (
-    <div>Dashboard</div>
+    <div>{syllabus.map((syllabi, i) => <p key={i}>{syllabi.title}</p>)}</div>
   );
 }
