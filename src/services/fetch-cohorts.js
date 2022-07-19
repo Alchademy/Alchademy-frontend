e69a3976c2f02ae805873b1f180985d241b6f3d8
Delaney Fogarty <delaney.fk@gmail.com>
2022-07-19 10:55:06 -0700

    cohort getbyUserId and getbyCohort id fetch functions

--- src/services/fetch-cohorts.js
+++ src/services/fetch-cohorts.js
@@ -1,12 +1,23 @@
-import { createClient } from '@supabase/supabase-js';
-export const client = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_KEY
-);
+export async function getCohortByUserId(user_id) {
+  const rawResponse = await fetch(`${process.env.REACT_APP_API_URL}/cohorts/user/${user_id}`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    mode: 'cors',
+  });
+  const data = await rawResponse.json();
 
-export function checkError({ data, error }) {
-  if (error) {
-    throw error;
-  }
+  return data;
+}
+
+export async function getCohortById(cohort_id) {
+  const rawResponse = await fetch(`${process.env.REACT_APP_API_URL}/cohorts/${cohort_id}`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    mode: 'cors',
+  });
+  const data = await rawResponse.json();
+
   return data;
 }
