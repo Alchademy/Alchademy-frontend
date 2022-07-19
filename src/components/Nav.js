ddbe69fea7ec098ef8de55fe171feb75b4634bd5
Beau Elliott <belliott1516@gmail.com>
2022-07-19 16:56:51 -0700

    updating navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import userEvent from '@testing-library/user-event';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { getUser } from '../services/fetch-users';
+import { useStateContext } from '../StateProvider';
+import './Nav.css';
 
 export default function Nav() {
+  const { user, setUser } = useStateContext({});
+
+  useEffect(() => {
+    async function getCurrentUser(){
+      const user = await getUser();
+      setUser(user);
+    }
+    getCurrentUser();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div>
+    <div className='navBar'>
+      <span className=''>
+        <p>Welcome {user.username}</p>
+      </span>
       <ul>
         <li>
           <Link to="/dashboard">Home</Link>
@@ -17,7 +35,9 @@ export default function Nav() {
         <li>
           <Link to="/assignments">Assignments</Link>
         </li>
+        <input className='navSearchBar'/>
       </ul>
+      
     </div>
   );
 }
