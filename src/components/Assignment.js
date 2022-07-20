bfd6a8caaa0657c69e80d1957fad76e6458f5665
Delaney Fogarty <delaney.fk@gmail.com>
2022-07-19 17:57:19 -0700

    got syllabus name appending

--- src/components/Assignment.js
+++ src/components/Assignment.js
@@ -1,19 +1,15 @@
-import { Link } from 'react-router-dom';
-
 export default function Assignment({ assgn }) {
   return (
-    <Link to={`/assignments/${assgn.id}`}>
-      <div className="assignment-list-div">
-        <h3>{assgn.title}</h3>
-        <p>
-          {assgn.due_date} | {assgn.total_points}{' '}
-        </p>
-        <p>
-          {' '}
-          {assgn.status_id === 2 && <span>Active</span>}
-          {assgn.status_id === 4 && <span>Completed</span>}{' '}
-        </p>
-      </div>
-    </Link>
+    <div className="assignment-list-div">
+      <h3>{assgn.title}</h3>
+      <p>
+        {assgn.due_date} | {assgn.total_points}{' '}
+      </p>
+      <p>
+        {' '}
+        {assgn.status_id === 2 && <span>Active</span>}
+        {assgn.status_id === 4 && <span>Completed</span>}{' '}
+      </p>
+    </div>
   );
 }
--- src/components/AssignmentList.js
+++ src/components/AssignmentList.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { getAssignmentsBySyllabusId } from '../services/fetch-assignments';
 import { useStateContext } from '../StateProvider';
 import Assignment from './Assignment';
 import './Assignments.css';
+import { getSyllabusByID } from '../services/fetch-syllabus';
 
 export default function AssignmentList() {
-  const { syllabus, assignment, getSyllabusAssignments, setAssignment, getSyllabus } =
+  const { syllabus, assignment, getSyllabusAssignments, setAssignment, setSyllabus } =
     useStateContext();
   const { id } = useParams();
 
@@ -17,17 +19,25 @@ export default function AssignmentList() {
         setAssignment(assignmentData);
       }
     }
+    async function getSyllabusName() {
+      const syllabus = await getSyllabusByID(id);
+      console.log(syllabus);
+      setSyllabus(syllabus);
+    }
     getAssignments();
-    getSyllabus();
+    getSyllabusName();
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [syllabus.id]);
 
   return (
     <div>
-      <div className="syllabus-title"> </div>
-      <div>
+      <div className="syllabus-title">{syllabus.title}</div>
+      <div className="assignment-list">
         {assignment.map((assgn, i) => (
-          <Assignment key={assgn.title + i} assgn={assgn} />
+          <Link className="assignment-tile" key={i + assgn.id} to={`/assignments/${assgn.id}`}>
+            <Assignment key={assgn.title + i} assgn={assgn} />
+          </Link>
         ))}
       </div>
     </div>
