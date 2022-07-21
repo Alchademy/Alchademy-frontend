b05592ceb93f266c188fbb945ec1be5657ccd0fe
Delaney Fogarty <delaney.fk@gmail.com>
2022-07-20 17:58:39 -0700

    converted date on assignments, added mui status_name badge

--- src/components/Assignment.js
+++ src/components/Assignment.js
@@ -3,14 +3,15 @@ import { Chip } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 export default function Assignment({ assgn }) {
+  const convertedTime = new Date(assgn.due_date).toLocaleDateString('en-us');
   return (
     <Link className="assignment-tile" to={`/assignments/${assgn.id}`}>
       <div className="assignment-list-div">
         <h3>{assgn.title}</h3>
-        <p>
-          {assgn.due_date} | {assgn.total_points}{' '}
-        </p>
+        <p>Due Date: {convertedTime}</p>
+        <p>Total Points: {assgn.total_points}</p>
         <Chip
+          style={{ margin: '10px' }}
           variant="outlined"
           color="success"
           label={assgn.status_id === 4 ? 'Completed' : 'Active'}
@@ -19,4 +20,8 @@ export default function Assignment({ assgn }) {
       </div>
     </Link>
   );
+  // working on getting custom colors for status name
+  // (assgn.status_id === 1 && '#dda438') ||
+  // (assgn.status_id === 2 && '#e89062') ||
+  // (assgn.status_id === 4 && '#1f4e5c')
 }
